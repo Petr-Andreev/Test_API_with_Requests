e369802aaa1bf136bb0a84f2b3ffb66531faa84a
Petr Andreev <drakula2805@gmail.com>
2024-06-11 15:46:49 +0300

    Creating a parent class and updating endpoints files

--- endpoinst/get_object.py
+++ endpoinst/get_object.py
@@ -1,15 +1,11 @@
 import requests
+from endpoinst.base_endpoint import Endpoint
 
-class GetObject:
-    response = None
-    response_json = None
+class GetObject(Endpoint):
 
     def get_by_id(self, id):
         self.response = requests.get(f"https://api.restful-api.dev/objects/{id}")
         self.response_json = self.response.json()
-
-    def check_response_is_200(self):
-        assert self.response.status_code == 200
 
     def check_response_id(self, object_id):
         assert self.response_json['id'] == object_id
